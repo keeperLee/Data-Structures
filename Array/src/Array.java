2acc27d9ac0e125a568e5ff0fd23f8c15ad945d3
toxicNthomas <leejamesjian@163.com>
2019-04-06 11:20:33 +0800

    为数组添加元素

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -32,4 +32,35 @@ public class Array {
         return size == 0;
     }
 
+    //向所有元素后添加一个新的元素
+    public void addLast(int e){
+      /*  if (size == data.length) {
+            throw new IllegalArgumentException("添加失败，数组已满！");
+        }
+        data[size] = e;
+        size ++ ;*/
+      //以上代码可以简化为以下代码：
+        add(size,e);
+    }
+    //在所有元素前添加一个新元素
+    public void addFirst(int e){
+        add(0,e);
+    }
+
+    //在第index个位置插入一个新元素e
+    public void add(int index , int e){
+        if (size == data.length) {
+            throw new IllegalArgumentException("添加失败，数组已满！");
+        }
+        if( index <  0 || index > size){
+            throw new IllegalArgumentException("添加失败，索引值不符合要求！");
+        }
+        for(int i = size - 1 ; i >= index ; i -- ){
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size ++;
+
+    }
+
 }
