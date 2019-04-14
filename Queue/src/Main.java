c8787d2cfb689273284603e3e34845088dc6dcf8
toxicNthomas <leejamesjian@163.com>
2019-04-14 22:26:56 +0800

    使用带尾指针的链表实现队列

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -20,14 +20,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int opCount = 1000000;
-//        ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
-//        double time1 = testQueue(arrayQueue,opCount);
-//        System.out.println("ArrayQueue, time: " + time1 + " s");
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
+        double time1 = testQueue(arrayQueue,opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
 
 
         LoopQueue<Integer> loopQueue = new LoopQueue<Integer>();
         double time2 = testQueue(loopQueue,opCount);
         System.out.println("LoopQueue, time: " + time2 + " s");
+
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<Integer>();
+        double time3 = testQueue(linkedListQueue,opCount);
+        System.out.println("LinkedListQueue, time: " + time3 + " s");
     }
 }
