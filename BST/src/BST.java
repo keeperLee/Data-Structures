aa15afb6305c73683ec293191e9d0d96a871fa1c
toxicNthomas <leejamesjian@163.com>
2019-05-09 15:30:26 +0800

    使用递归的方式实现二分搜索树的插入元素操作

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+/**
+ * 在此实现的二分搜索树不包含重复元素，如果需要包含重复元素则需要定义：左子树小于等于节点；或者右子树大于等于节点
+ * @param <E>
+ */
+public class BST<E extends Comparable<E>> {
 
-public class BST {
+    private class Node{
+        E e;
+        Node left , right;
+
+        public Node(E e){
+            this.e = e;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    //根节点
+    private Node root;
+
+    //元素个数
+    private int size;
+
+    public BST(){
+        root = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0 ;
+    }
+
+    //向二分搜索树中添加新的元素e
+    public void add(E e){
+        if(root == null){
+            root = new Node(e);
+            size++;
+        }else{
+            add(root ,e);
+        }
+    }
+    //向以node为根的二分搜索树中插入元素E，递归算法
+    //如果递归终止条件不满足，则进行递归调用，直到递归终止条件满足，退出递归调用
+    private void add(Node node , E e){
+        //递归终止条件
+        if(e.equals(node.e)){
+            return ;
+        }else if(e.compareTo(node.e) < 0 && node.left == null){
+            node.left = new Node(e);
+            size++;
+            return ;
+        }else if(e.compareTo(node.e) > 0 && node.right == null){
+            node.right = new Node(e);
+            size++;
+            return ;
+        }
+
+        //递归调用
+        if(e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        }else{
+            add(node.right,e);
+        }
+
+    }
 }
