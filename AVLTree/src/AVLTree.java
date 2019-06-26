157df4abbe65e5c150a2c7d4f2ba7c50f9825c8e
toxicNthomas <leejamesjian@163.com>
2019-06-26 19:44:23 +0800

    AVL树

--- AVLTree/src/AVLTree.java
+++ AVLTree/src/AVLTree.java
@@ -6,14 +6,14 @@ public class AVLTree<K extends Comparable<K>, V> {
         public K key;
         public V value;
         public Node left, right;
-        public int height;   //节点所处的高度
+        public int height;
 
         public Node(K key, V value){
             this.key = key;
             this.value = value;
             left = null;
             right = null;
-            height = 1;   //新的节点，叶子节点初始高度为1；
+            height = 1;
         }
     }
 
@@ -33,20 +33,102 @@ public class AVLTree<K extends Comparable<K>, V> {
         return size == 0;
     }
 
-    //传入一个节点，返回它的高度值
+    // 判断该二叉树是否是一棵二分搜索树
+    public boolean isBST(){
+
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+        for(int i = 1 ; i < keys.size() ; i ++)
+            if(keys.get(i - 1).compareTo(keys.get(i)) > 0)
+                return false;
+        return true;
+    }
+
+    private void inOrder(Node node, ArrayList<K> keys){
+
+        if(node == null)
+            return;
+
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+    }
+
+    // 判断该二叉树是否是一棵平衡二叉树
+    public boolean isBalanced(){
+        return isBalanced(root);
+    }
+
+    // 判断以Node为根的二叉树是否是一棵平衡二叉树，递归算法
+    private boolean isBalanced(Node node){
+
+        if(node == null)
+            return true;
+
+        int balanceFactor = getBalanceFactor(node);
+        if(Math.abs(balanceFactor) > 1)
+            return false;
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
+    // 获得节点node的高度
     private int getHeight(Node node){
-        if(node == null){
+        if(node == null)
             return 0;
-        }
         return node.height;
     }
 
-    //计算每一个节点node的平衡因子
+    // 获得节点node的平衡因子
     private int getBalanceFactor(Node node){
-        if(node == null){
+        if(node == null)
             return 0;
-        }
         return getHeight(node.left) - getHeight(node.right);
+    }
+
+    // 对节点y进行向右旋转操作，返回旋转后新的根节点x
+    //        y                              x
+    //       / \                           /   \
+    //      x   T4     向右旋转 (y)        z     y
+    //     / \       - - - - - - - ->    / \   / \
+    //    z   T3                       T1  T2 T3 T4
+    //   / \
+    // T1   T2
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+
+        // 向右旋转过程
+        x.right = y;
+        y.left = T3;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    // 对节点y进行向左旋转操作，返回旋转后新的根节点x
+    //    y                             x
+    //  /  \                          /   \
+    // T1   x      向左旋转 (y)       y     z
+    //     / \   - - - - - - - ->   / \   / \
+    //   T2  z                     T1 T2 T3 T4
+    //      / \
+    //     T3 T4
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+
+        // 向左旋转过程
+        x.left = y;
+        y.right = T2;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
     }
 
     // 向二分搜索树中添加新的元素(key, value)
@@ -70,14 +152,29 @@ public class AVLTree<K extends Comparable<K>, V> {
         else // key.compareTo(node.key) == 0
             node.value = value;
 
-        //更新height
-        node.height = 1 + Math.max(getHeight(node.left),getHeight(node.right));
+        // 更新height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
 
-        //计算平衡因子
+        // 计算平衡因子
         int balanceFactor = getBalanceFactor(node);
-        if(Math.abs(balanceFactor )> 1){
-            System.out.println("unbalanced: " + balanceFactor);
+
+        // 平衡维护
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+            return leftRotate(node);
+
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
         }
+
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
         return node;
     }
 
@@ -195,11 +292,11 @@ public class AVLTree<K extends Comparable<K>, V> {
 
         System.out.println("Pride and Prejudice");
 
-        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<>();
         if(FileOperation.readFile("pride-and-prejudice.txt", words)) {
             System.out.println("Total words: " + words.size());
 
-            AVLTree<String, Integer> map = new AVLTree<String, Integer>();
+            AVLTree<String, Integer> map = new AVLTree<>();
             for (String word : words) {
                 if (map.contains(word))
                     map.set(word, map.get(word) + 1);
@@ -210,6 +307,9 @@ public class AVLTree<K extends Comparable<K>, V> {
             System.out.println("Total different words: " + map.getSize());
             System.out.println("Frequency of PRIDE: " + map.get("pride"));
             System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+
+            System.out.println("is BST : " + map.isBST());
+            System.out.println("is Balanced : " + map.isBalanced());
         }
 
         System.out.println();
